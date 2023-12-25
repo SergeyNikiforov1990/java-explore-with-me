f24882a5e3a9180446a983394be43fc2b470f4cb
SergeyNikiforov1990 <SergeyNikiforov32@yandex.ru>
2023-12-26 00:50:56 +0500

    feature_location_processing v.1.1

--- main-service/src/main/java/ru/practicum/main_service/categories/controller/AdminCategoriesController.java
+++ main-service/src/main/java/ru/practicum/main_service/categories/controller/AdminCategoriesController.java
@@ -23,6 +23,7 @@ public class AdminCategoriesController {
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryDto createCategories(@RequestBody @Valid NewCategoryDto newCategoryDto) {
         return categoriesService.createCategories(newCategoryDto);
+
     }
 
     @DeleteMapping("/{catId}")
